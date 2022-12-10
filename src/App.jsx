9c6fe3788604b72e61851cf33b037583fe468412
sunfireaegis <henry_morgan06@mail.ru>
2022-12-10 10:47:42 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import { Component } from 'react';
+import './App.css';
+import Inbox from './endpoints/Inbox'
+
+class App extends Component {
+  render () {
+    return (
+      <>
+        <Inbox/>
+      </>
+    )
+  }
+}
+
+export default App
---
+++ src/endpoints/Inbox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Component } from "react";
+
+
+class Inbox extends Component {
+    render () {
+        return (
+            <span>123</span>
+        )
+    }
+}
+
+export default Inbox
